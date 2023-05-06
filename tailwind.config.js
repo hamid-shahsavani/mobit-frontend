dd9893cd199cf5e5ba63de366be881910bfa5e4f
sys113 <051.sys113@gmail.com>
2023-05-07 04:12:50 +0430

    add base color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // global
+        'base-white': '#FFFFFF',
+        'base-green': '#66BB6A',
+        'base-red': '#FF6A6A',
+        'base-yellow': '#FFBD5C',
+        'base-purple': '#B183F8',
+        'base-royal-blue': '#377DFF',
+        'base-sky-blue': '#55E0F6',
+        'base-gray-100': '#F5F5F5',
+        'base-gray-200': '#DDDDDD',
+        'base-gray-300': '#ABB4BC',
+        'base-gray-400': '#5D6670',
+        'base-gray-500': '#3F434D',
+        'base-gray-600': '#1E2022',
       },
     },
   },
   plugins: [],
-}
+};
